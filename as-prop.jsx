7c9f5f3ba50846d7bad55148f15b02be6030e799
Andrew Jose <ajosedev@gmail.com>
2019-11-14 14:12:14 +1100

    As prop

---
+++ as-prop.jsx
@@ -0,0 +1,13 @@
+// Use 'as' prop
+const Grid = ({ as: Element, ...props }) => (
+  <Element className="grid" {...props} />,
+);
+
+Grid.defaultProps = {
+  as: 'div',
+};
+
+// Can call as
+<Grid as="main">
+  <div />
+</Grid>
