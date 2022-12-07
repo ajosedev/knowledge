802c7ea2534ec9419b7d349d93b0f920bb046447
Andrew Jose <ajosedev@gmail.com>
2022-12-07 21:06:37 +1100

    Change code theme

--- src/gatsby-theme-garden/components/mdx-components/CodeBlock.jsx
+++ src/gatsby-theme-garden/components/mdx-components/CodeBlock.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export function CodeBlock({ className, ...props }) {
   const match = /language-(\w+)/.exec(className || '')
+
   return match
-    ? <SyntaxHighlighter language={match[1]} PreTag="div" style={stackoverflowDark} {...props} />
+    ? <SyntaxHighlighter language={match[1]} PreTag="div" style={atomOneDark} {...props} />
     : <code className={className} {...props} />
 }
