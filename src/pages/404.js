b2935c8b818dd06602e47437c84c2cc8ec5ccc24
Andrew Jose <ajosedev@gmail.com>
2023-03-31 23:34:58 +1100

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export default function Component() {
-  return <div>404 - Page not found</div>
+  return (
+    <div>
+      <h1>404 - Page not found</h1>
+      <p>
+        This probably means you've clicked on a link to a note that doesn't
+        exist yet. Sorry!
+      </p>
+    </div>
+  );
 }
